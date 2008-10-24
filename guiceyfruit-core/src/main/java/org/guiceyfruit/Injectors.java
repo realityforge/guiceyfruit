758133087e8c9e7270315fa98091507a9798b1c3
James Strachan <james.strachan@gmail.com>
2008-10-24 19:31:21 +0000

    more consistent generics use

--- guiceyfruit-core/src/main/java/org/guiceyfruit/Injectors.java
+++ guiceyfruit-core/src/main/java/org/guiceyfruit/Injectors.java
@@ -26,6 +26,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.Provider;
 import com.google.inject.TypeLiteral;
 import com.google.inject.matcher.Matcher;
 import com.google.inject.name.Names;
@@ -120,6 +121,85 @@ public class Injectors {
         Binding<?> binding = entry.getValue();
         Object value = binding.getProvider().get();
         answer.add((T) value);
+      }
+    }
+    return answer;
+  }
+
+  /**
+   * Returns a collection of all of the providers matching the given matcher
+   *
+   * @param matcher matches the types to return instances
+   * @return a set of objects returned from this injector
+   */
+  public static <T> Set<Provider<T>> getProvidersOf(Injector injector, Matcher<Class> matcher) {
+    Set<Provider<T>> answer = Sets.newHashSet();
+    Set<Entry<Key<?>, Binding<?>>> entries = injector.getBindings().entrySet();
+    for (Entry<Key<?>, Binding<?>> entry : entries) {
+      Key<?> key = entry.getKey();
+      Class<?> keyType = getKeyType(key);
+      if (keyType != null && matcher.matches(keyType)) {
+        Binding<?> binding = entry.getValue();
+        answer.add((Provider<T>) binding.getProvider());
+      }
+    }
+    return answer;
+  }
+
+  /**
+   * Returns a collection of all providers of the given base type
+   *
+   * @param baseClass the base type of objects required
+   * @param <T> the base type
+   * @return a set of objects returned from this injector
+   */
+  public static <T> Set<Provider<T>> getProvidersOf(Injector injector, Class<T> baseClass) {
+    Set<Provider<T>> answer = Sets.newHashSet();
+    Set<Entry<Key<?>, Binding<?>>> entries = injector.getBindings().entrySet();
+    for (Entry<Key<?>, Binding<?>> entry : entries) {
+      Key<?> key = entry.getKey();
+      Class<?> keyType = getKeyType(key);
+      if (keyType != null && baseClass.isAssignableFrom(keyType)) {
+        Binding<?> binding = entry.getValue();
+        answer.add((Provider<T>) binding.getProvider());
+      }
+    }
+    return answer;
+  }
+
+  /**
+   * Returns a collection of all of the bindings matching the given matcher
+   *
+   * @param matcher matches the types to return instances
+   * @return a set of objects returned from this injector
+   */
+  public static Set<Binding<?>> getBindingsOf(Injector injector, Matcher<Class> matcher) {
+    Set<Binding<?>> answer = Sets.newHashSet();
+    Set<Entry<Key<?>, Binding<?>>> entries = injector.getBindings().entrySet();
+    for (Entry<Key<?>, Binding<?>> entry : entries) {
+      Key<?> key = entry.getKey();
+      Class<?> keyType = getKeyType(key);
+      if (keyType != null && matcher.matches(keyType)) {
+        answer.add(entry.getValue());
+      }
+    }
+    return answer;
+  }
+
+  /**
+   * Returns a collection of all bindings of the given base type
+   *
+   * @param baseClass the base type of objects required
+   * @return a set of objects returned from this injector
+   */
+  public static Set<Binding<?>> getBindingsOf(Injector injector, Class<?> baseClass) {
+    Set<Binding<?>> answer = Sets.newHashSet();
+    Set<Entry<Key<?>, Binding<?>>> entries = injector.getBindings().entrySet();
+    for (Entry<Key<?>, Binding<?>> entry : entries) {
+      Key<?> key = entry.getKey();
+      Class<?> keyType = getKeyType(key);
+      if (keyType != null && baseClass.isAssignableFrom(keyType)) {
+        answer.add(entry.getValue());
       }
     }
     return answer;
