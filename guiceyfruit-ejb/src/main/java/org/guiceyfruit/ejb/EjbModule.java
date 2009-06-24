fd154e44acd918d833a12aa9903b61cdc7bc48a6
James Strachan <james.strachan@gmail.com>
2009-06-24 13:35:30 +0000

    added some javadoc

--- guiceyfruit-ejb/src/main/java/org/guiceyfruit/ejb/EjbModule.java
+++ guiceyfruit-ejb/src/main/java/org/guiceyfruit/ejb/EjbModule.java
@@ -23,6 +23,8 @@ import org.guiceyfruit.ejb.support.EJBMemberProvider;
 import javax.ejb.EJB;
 
 /**
+ * Allows objects to be injected using the {@link EJB} annotation
+ *
  * @version $Revision: 1.1 $
  */
 public class EjbModule  extends Jsr250Module {
