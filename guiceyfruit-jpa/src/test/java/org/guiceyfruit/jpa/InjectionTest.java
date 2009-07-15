f4ed17ce80791d5d89031db1e276cedd2e8d9cc4
James Strachan <james.strachan@gmail.com>
2009-07-15 14:08:01 +0000

    improved test case to show how to inject an EntityManager based on an EntityManagerFactory implementation

--- guiceyfruit-jpa/src/test/java/org/guiceyfruit/jpa/InjectionTest.java
+++ guiceyfruit-jpa/src/test/java/org/guiceyfruit/jpa/InjectionTest.java
@@ -20,153 +20,186 @@ package org.guiceyfruit.jpa;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
+import junit.framework.TestCase;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.FlushModeType;
 import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import junit.framework.TestCase;
+import java.util.Map;
 
-/** @version $Revision: 1.1 $ */
+/**
+ * @version $Revision: 1.1 $
+ */
 public class InjectionTest extends TestCase {
 
-  public void testInjection() throws Exception {
-    final EntityManager stubEntityManager = createStubEntityManager();
+    public void testInjection() throws Exception {
+        final EntityManager stubEntityManager = createStubEntityManager();
 
-    Injector injector = Guice.createInjector(new JpaModule() {
-      @Override
-      protected void configure() {
-        super.configure();
+        Injector injector = Guice.createInjector(new JpaModule() {
 
-        bind(EntityManager.class).toInstance(stubEntityManager);
-      }
-    });
+            @Override
+            protected void configure() {
+                super.configure();
 
-    TestDTO testDTO = injector.getInstance(TestDTO.class);
-    assertEquals("EntityManager", stubEntityManager, testDTO.getEntityManager());
-  }
+                // TODO this should be the hibernate implementation
+                bind(EntityManagerFactory.class).toInstance(
+                        new EntityManagerFactory() {
+                            boolean open = true;
 
-  protected EntityManager createStubEntityManager() {
-    return new EntityManager() {
-      public void persist(Object o) {
-        // TODO
+                            public EntityManager createEntityManager() {
+                                return stubEntityManager;
+                            }
 
-      }
+                            public EntityManager createEntityManager(Map map) {
+                                return stubEntityManager;
+                            }
 
-      public <T> T merge(T t) {
-        // TODO
-        return null;
-      }
+                            public void close() {
+                                open = false;
+                            }
 
-      public void remove(Object o) {
-        // TODO
+                            public boolean isOpen() {
+                                return open;
+                            }
+                        }
+                );
+            }
 
-      }
+            @Provides
+            public EntityManager createEntityManager(EntityManagerFactory entityManagerFactory) {
+                return entityManagerFactory.createEntityManager();
+            }
+        });
 
-      public <T> T find(Class<T> tClass, Object o) {
-        // TODO
-        return null;
-      }
-
-      public <T> T getReference(Class<T> tClass, Object o) {
-        // TODO
-        return null;
-      }
-
-      public void flush() {
-        // TODO
-
-      }
-
-      public void setFlushMode(FlushModeType flushModeType) {
-        // TODO
-
-      }
-
-      public FlushModeType getFlushMode() {
-        // TODO
-        return null;
-      }
-
-      public void lock(Object o, LockModeType lockModeType) {
-        // TODO
-
-      }
-
-      public void refresh(Object o) {
-        // TODO
-
-      }
-
-      public void clear() {
-        // TODO
-
-      }
-
-      public boolean contains(Object o) {
-        // TODO
-        return false;
-      }
-
-      public Query createQuery(String s) {
-        // TODO
-        return null;
-      }
-
-      public Query createNamedQuery(String s) {
-        // TODO
-        return null;
-      }
-
-      public Query createNativeQuery(String s) {
-        // TODO
-        return null;
-      }
-
-      public Query createNativeQuery(String s, Class aClass) {
-        // TODO
-        return null;
-      }
-
-      public Query createNativeQuery(String s, String s1) {
-        // TODO
-        return null;
-      }
-
-      public void close() {
-        // TODO
-
-      }
-
-      public boolean isOpen() {
-        // TODO
-        return false;
-      }
-
-      public EntityTransaction getTransaction() {
-        // TODO
-        return null;
-      }
-
-      public void joinTransaction() {
-        // TODO
-
-      }
-
-      public Object getDelegate() {
-        // TODO
-        return null;
-      }
-    };
-  }
-
-  public static class TestDTO {
-    @PersistenceContext
-    EntityManager entityManager;
-
-    public EntityManager getEntityManager() {
-      return entityManager;
+        TestDTO testDTO = injector.getInstance(TestDTO.class);
+        assertEquals("EntityManager", stubEntityManager, testDTO.getEntityManager());
     }
-  }
+
+    protected EntityManager createStubEntityManager() {
+        return new EntityManager() {
+            public void persist(Object o) {
+                // TODO
+
+            }
+
+            public <T> T merge(T t) {
+                // TODO
+                return null;
+            }
+
+            public void remove(Object o) {
+                // TODO
+
+            }
+
+            public <T> T find(Class<T> tClass, Object o) {
+                // TODO
+                return null;
+            }
+
+            public <T> T getReference(Class<T> tClass, Object o) {
+                // TODO
+                return null;
+            }
+
+            public void flush() {
+                // TODO
+
+            }
+
+            public void setFlushMode(FlushModeType flushModeType) {
+                // TODO
+
+            }
+
+            public FlushModeType getFlushMode() {
+                // TODO
+                return null;
+            }
+
+            public void lock(Object o, LockModeType lockModeType) {
+                // TODO
+
+            }
+
+            public void refresh(Object o) {
+                // TODO
+
+            }
+
+            public void clear() {
+                // TODO
+
+            }
+
+            public boolean contains(Object o) {
+                // TODO
+                return false;
+            }
+
+            public Query createQuery(String s) {
+                // TODO
+                return null;
+            }
+
+            public Query createNamedQuery(String s) {
+                // TODO
+                return null;
+            }
+
+            public Query createNativeQuery(String s) {
+                // TODO
+                return null;
+            }
+
+            public Query createNativeQuery(String s, Class aClass) {
+                // TODO
+                return null;
+            }
+
+            public Query createNativeQuery(String s, String s1) {
+                // TODO
+                return null;
+            }
+
+            public void close() {
+                // TODO
+
+            }
+
+            public boolean isOpen() {
+                // TODO
+                return false;
+            }
+
+            public EntityTransaction getTransaction() {
+                // TODO
+                return null;
+            }
+
+            public void joinTransaction() {
+                // TODO
+
+            }
+
+            public Object getDelegate() {
+                // TODO
+                return null;
+            }
+        };
+    }
+
+    public static class TestDTO {
+        @PersistenceContext
+        EntityManager entityManager;
+
+        public EntityManager getEntityManager() {
+            return entityManager;
+        }
+    }
 }
